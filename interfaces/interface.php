13f5639db30cd08806a44a69ed6a9f4ce9ea9181
unknown <willianminn@gmail.com>
2018-07-16 00:33:09 +0630

    09-Extend an interface

---
+++ interfaces/interface.php
@@ -0,0 +1,19 @@
+<?php
+interface TableInterface{
+    public function save(array $data);
+}
+interface LogInterface{
+    public function log($message);
+}
+class Table implements TableInterface,LogInterface {
+    public function save(array $data)
+    {
+        return 'foo ';
+    }
+    public function log($message)
+    {
+        return $message."\n";
+    }
+}
+echo (new Table())->save([]);
+echo (new Table())->log('Interfaces are awesome');
